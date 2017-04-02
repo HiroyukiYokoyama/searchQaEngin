65e543625ee7885ebe6352e5c475ec2fd39c0f24
hiroyuki.yokoyama <hiroyuki.yokoyama.3@gmail.com>
2017-04-02 18:26:38 +0900

    [add,change]作業中

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 function search() {
     var searchCriteria = encodeURI("body:" + $("#criteria").val());
-    console.log(searchCriteria);
+    // Qiitaの検索API実行
+    searchQiita(searchCriteria);
+    // Teratailの検索API実行
+    searchTera(searchCriteria);
+}
+
+function searchQiita(searchCriteria){
     var url = "https://qiita.com/api/v2/items?page=1&per_page=20&query=" + searchCriteria;
     $.ajax({
         type: "GET",
@@ -20,6 +26,30 @@ function search() {
                             + "</tr>"
             }
             qiitabody.append(html);
+        }
+    });
+}
+
+function searchTera(searchCriteria){
+    var url = "https://teratail.com/api/v1/questions?limit=20&page=1&body_str=java" + searchCriteria;
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (result) {
+            var questions = result.questions;
+            var html = "";
+            var teraBody = $("#teraBody");
+            teraBody.empty()
+            for(var i = 0 ; i < questions.length  ; i++){
+                var curRes =  questions[i];
+                var no = i + 1;
+                var targetUrl = "'" + curRes.url +"'";
+                html = html + '<tr onclick="onTr(' + targetUrl + ')">'
+                            +   "<td style='width:5%'>" + no + "</td>"
+                            +   "<td style='width:45%'>" + curRes.title + "</td>"
+                            + "</tr>"
+            }
+            teraBody.append(html);
             console.log(result);
         }
     });
